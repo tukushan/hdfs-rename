3c40dfa85a8fa0348ec7b48e6a314115ebbdebfb
Oliver Mannion <125105+tukushan@users.noreply.github.com>
2018-02-19 10:29:00 +1100

    use relative (rather than hard-coded) classpath

--- hdfs-rename-dist/src/main/bin/hdfs-rename.sh
+++ hdfs-rename-dist/src/main/bin/hdfs-rename.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java -cp "/opt/tenaris/hdfs-rename/lib/*" com.tenaris.bigdata.hdfs.renamer.RenameCli "$@"
+LIB_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../lib
+echo $LIB_DIR
+java -cp "$LIB_DIR/*" com.tenaris.bigdata.hdfs.renamer.RenameCli "$@"
